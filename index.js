2bd7eccbc6e3a444b8cb2754b0654b916d2ede61
piguyisme <piguyisme@gmail.com>
2022-01-10 16:49:12 -0800

    more client side

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
+/*jshint esversion:8 */
 const express = require('express');
 
 const app = express();
 
-class authorizedBNetConnection {
+require('dotenv').config();
+
+class AuthorizedBNetConnection {
+  constructor() {
+    this.connectCallback = null;
+  }
   async requestAPI(endpoint, method, options = {}) {
     let config = options;
-    config["method"] = method;
-    config["url"] = `https://www.bungie.net/platform${endpoint}`;
-    config["headers"]["X-Api-Key"] = process.env.api_key;
+    config.method = method;
+    config.url = `https://www.bungie.net/platform${endpoint}`;
+    config.headers["X-Api-Key"] = process.env.api_key;
     if (this.accessToken) {
-      config["headers"]["Authorization"] = `Bearer ${this.accessToken}`;
+      config.headers["Authorization"] = `Bearer ${this.accessToken}`;
     }
     axios(config).then(res => {
       return res;
     }).catch(e => {
       console.error(e);
-    })
+    });
   }
-  connect(authCode) {
+  connect(auth_code) {
     if (!this.accessToken) {
       this.requestAPI('/app/oauth/token/', 'post', {
         params: {
           "grant_type": "authorization_code",
-          "code": authCode
+          "code": auth_code
         },
         headers: {
           "Content-Type": "application/x-www-form-urlencoded"
@@ -30,10 +36,10 @@ class authorizedBNetConnection {
       }).then(data => {
         console.log(data.code);
         this.accessToken = data.code;
-        this.connectCallback;
+        this.connectCallback();
       }).catch(e=>{
         console.error(e);
-      })
+      });
     }
   }
   getMembershipID() {
@@ -50,7 +56,7 @@ class authorizedBNetConnection {
 
 const axios = require('axios');
 
-const bAPI = new authorizedBNetConnection;
+const bAPI = new AuthorizedBNetConnection();
 
 let authCode;
 app.get('/', (req, res) => {
@@ -58,11 +64,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/callback', (req, res) => {
-  authCode = req.query.code;
-  bAPI.connect(authCode);
-})
+  res.sendFile(__dirname + '/index.html');
+  // authCode = req.query.code;
+  // bAPI.connect(authCode);
+});
 
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log('server started');
 });
 
@@ -79,5 +86,5 @@ function makeid(length) {
 }
 
 bAPI.on('connect', () => {
-  console.log(bAPI.accessToken)
-})
+  console.log(bAPI.accessToken);
+});
